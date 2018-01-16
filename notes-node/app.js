31df9fa821c2bb96d97c103d8283cad27601c1bb
Kyle Archer <kaa.irish@gmail.com>
2018-01-16 14:46:56 -0700

    Finish video 10

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,14 +1,17 @@
 /*jshint esversion: 6 */
 
-console.log('Starting App');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
-var user = os.userInfo();
+console.log('Result:', notes.add(9, -2));
 
-fs.appendFile('greatings.txt', `Hello ${user.username}!`, function(err) {
-    if (err) {
-        console.log("Unable to write to file");
-    }
-});
+// var user = os.userInfo();
+
+// fs.appendFile('greatings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err) {
+//     if (err) {
+//         console.log("Unable to write to file");
+//     }
+// });
---
+++ notes-node/notes.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+console.log("Starting notes.js");
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
