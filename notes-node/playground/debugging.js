f2106ecbded991ca3b5191098c7ae5847d44e9d3
Kyle Archer <kaa.irish@gmail.com>
2018-02-06 08:30:57 -0700

    video 20 and 21

---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,15 @@
+var person = {
+    name: 'Kyle'
+};
+
+person.age = 27;
+
+// node inspect
+// c to continue
+// n to go to the next line
+// repl to enter into interactive terminal
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
