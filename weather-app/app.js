3e9e6505bfaf022340e8847c177187570ec5dd90
Kyle Archer <kaa.irish@gmail.com>
2018-02-15 11:00:05 -0700

    geocode file, video 33

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,8 @@
 /*jshint esversion: 6 */
 
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -16,20 +17,11 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-console.log(encodedAddress);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find address');
-    } else if (body.status === 'OK') {
-        console.log(`Address ${body.results[0].formatted_address}`);
-        console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-        console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+console.log(argv.address);
+geocode.geocodeAddress(argv.address, (errorMessage, result) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
