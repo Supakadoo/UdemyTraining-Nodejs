6cd8871bd186086dc9c39ef265bc77fead6846b0
Kyle Archer <kaa.irish@gmail.com>
2018-02-16 09:13:39 -0700

    get weather, video 34

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,44 @@
 /*jshint esversion: 6 */
 
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-})
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+// })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-console.log(argv.address);
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+// abb9baa14155cd2533c6f4a053272d92
+// https://api.darksky.net/forecast/abb9baa14155cd2533c6f4a053272d92/40.3363082,-111.9880199
+
+const request = require('request');
+
+request({
+    url: `https://api.darksky.net/forecast/abb9baa14155cd2533c6f4a053272d92/40.3363082,-111.9880199`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Unable to connect to dark sky servers');
+    } else if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);   
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('Unable to fetch weather');
     }
 });
