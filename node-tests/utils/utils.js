d16b490061fc2396a73b301dfea225f849dce169
Kyle Archer <kaa.irish@gmail.com>
2018-05-14 09:15:40 -0600

    go over testing

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -2,4 +2,23 @@
 
 module.exports.add = (a, b) => a + b;
 
+module.exports.asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+};
+
 module.exports.square = (x) => x * x;
+
+module.exports.asyncSquare = (x, callback) => {
+    setTimeout(() => {
+        callback(x * x);
+    }, 1000);
+};
+
+module.exports.setName = (user, fullName) => {
+    var names = fullName.split(' ');
+    user.firstName = name[0];
+    user.lastName = name[1];
+    return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,42 @@
 /*jshint esversion: 6 */
 
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33,11);
-    if (res != 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44);
+    expect(typeof res).toBe('number');
 });
 
-it('should quare a numbers', () => {
-    var res = utils.square(11);
-    if (res != 121) {
-        throw new Error(`Expected 121, but got ${res}.`);
-    }
+it('should add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7);
+        expect(typeof sum).toBe('number');
+        done();
+    });
 });
+
+it('should square a number', () => {
+    var res = utils.square(11);
+    expect(res).toBe(121);
+    expect(typeof res).toBe('number');
+});
+
+it('should square a number', (done) => {
+    utils.asyncSquare(3, (square) => {
+        expect(square).toBe(9);
+        expect(typeof square).toBe('number');
+        done();
+    });
+});
+
+// it ('should be full name', () => {
+//     var user = {location: 'Utah', age: 27};
+//     var res = utils.setName(user, 'Kyle Archer');
+
+//     expect(res.toObject().tokens[0]).toMatchObject({
+//         firstName: 'Kyle',
+//         lastName: 'Archer'
+//     });
+// });
