b6b1979fadcb9d53aaea47dc82397bb84181bc6f
Kyle Archer <kaa.irish@gmail.com>
2018-05-15 10:43:09 -0600

    test express, video 26

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -5,7 +5,22 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Bob',
+        age: 99
+    }, {
+        name: 'Kyle',
+        age: 27
+    }]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
