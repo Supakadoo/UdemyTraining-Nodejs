962492550c2fc12ea6e919552e238cc79378a5a9
Kyle Archer <kaa.irish@gmail.com>
2018-05-16 09:00:54 -0600

    video 57, add describe blocks

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -5,27 +5,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        // .expect((res) => {
-        //     expect(res.body).toInclude({
-        //         error: 'Page not found.'
-        //     });
-        // })
-        .end(done);
-});
+describe('Server', () => {
 
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        // .expect((res) => {
-        //     expect(res.body).toInclude({
-        //         user: 'Kyle',
-        //         age: 27
-        //     });
-        // })
-        .end(done);
+    describe('Get /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                // .expect((res) => {
+                //     expect(res.body).toInclude({
+                //         error: 'Page not found.'
+                //     });
+                // })
+                .end(done);
+        });
+    });
+    
+    describe('Get /users', () => {
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                // .expect((res) => {
+                //     expect(res.body).toInclude({
+                //         user: 'Kyle',
+                //         age: 27
+                //     });
+                // })
+                .end(done);
+        });
+    });
+
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,31 +3,38 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33,11);
-    expect(res).toBe(44);
-    expect(typeof res).toBe('number');
-});
+describe('Utils', () => {
 
-it('should add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7);
-        expect(typeof sum).toBe('number');
-        done();
+    describe('#Add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33,11);
+            expect(res).toBe(44);
+            expect(typeof res).toBe('number');
+        });
+        
+        it('should add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7);
+                expect(typeof sum).toBe('number');
+                done();
+            });
+        });
     });
-});
-
-it('should square a number', () => {
-    var res = utils.square(11);
-    expect(res).toBe(121);
-    expect(typeof res).toBe('number');
-});
-
-it('should square a number', (done) => {
-    utils.asyncSquare(3, (square) => {
-        expect(square).toBe(9);
-        expect(typeof square).toBe('number');
-        done();
+    
+    describe('#Square', () => {
+        it('should square a number', () => {
+            var res = utils.square(11);
+            expect(res).toBe(121);
+            expect(typeof res).toBe('number');
+        });
+        
+        it('should square a number', (done) => {
+            utils.asyncSquare(3, (square) => {
+                expect(square).toBe(9);
+                expect(typeof square).toBe('number');
+                done();
+            });
+        });
     });
 });
 
