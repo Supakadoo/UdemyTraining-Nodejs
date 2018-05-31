10dccfeffa0a78093a5073bba26b8c1c1e419fe7
Kyle Archer <kaa.irish@gmail.com>
2018-05-31 09:10:12 -0600

    video 63, app interacting with mongodb

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6 */
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'Kyle Archer',
+        age: 27,
+        location: 'Utah'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert Users', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    client.close();
+});
