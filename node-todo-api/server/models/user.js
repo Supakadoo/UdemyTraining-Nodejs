bff05a8982e1db7e7226bffb21b52b5bc420c89e
Kyle Archer <kaa.irish@gmail.com>
2018-06-14 09:40:30 -0600

    video 73, more modular use express

---
+++ node-todo-api/server/models/user.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    }
+});
+
+module.exports = {User};
